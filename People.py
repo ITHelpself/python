940873819592fb68189e772521ed74eab4a7b3e3
phithai <phithai289@gmail.com>
2020-07-10 09:12:25 +0700

    sử dụng itemgetter, attrgetter để hỗ trợ sắp xếp nhanh

---
+++ People.py
@@ -0,0 +1,8 @@
+from operator import itemgetter, attrgetter
+people = [{'name':'chandan','age':20,'salary':2000},
+{'name':'chetan','age':18,'salary':5000},
+{'name':'guru','age':30,'salary':3000}]
+by_age = itemgetter('age')
+by_salary = itemgetter('salary')
+people.sort(key=by_age) #in-place sorting by age
+people.sort(key=by_salary) #in-place sorting by salary
