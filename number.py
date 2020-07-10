9213f55d2064c727e6a6c3a99b8ee0effcd1cc50
phithai <phithai289@gmail.com>
2020-07-10 09:44:15 +0700

    # sử dụng operator dể tính các số nguyên math số thực cmath số complex

--- number.py
+++ number.py
@@ -1,4 +1,7 @@
-// truy cập phần tử mảng
+import operator
+import math
+import cmath
+# truy cập phần tử mảng
 lst = [1,2,3,4]
 lst[1:] # [2, 3, 4]
 lst[:3] # [1, 2, 3]
@@ -7,3 +10,7 @@ lst[::-1] # [4, 3, 2, 1]
 lst[-1:0:-1] # [4, 3, 2]
 lst[5:8] # [] since starting index is greater than length of lst, returns empty list
 lst[1:10] # [2, 3, 4] same as omitting ending index
+# phép toán các số
+num = operator.add(2,3)
+num = math.sqrt(45)
+num = cmath.sqrt(34)
