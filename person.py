2b90d88b14d238c65e6ef9cb830a546a650cdcab
phithai <phithai289@gmail.com>
2020-07-11 22:25:46 +0700

    # kế thừa

--- person.py
+++ person.py
@@ -1,9 +1,8 @@
 from operator import itemgetter,attrgetter
 class Person:
     """a simple class"""
-    @classmethod
-    def __init__(self):
-        self.name = "default"
+    def __init__(self,name):
+        self.name = name
         Person.count = 0
     def __str__(self):
         return self.name
@@ -15,6 +14,12 @@ class Person:
     @staticmethod
     def getCount():
         return Person.count
+class SpecialPerson(Person):
+    def __init__(self, name, rewardMark):
+        super(SpecialPerson, self).__init__(name)
+        self.rewardMark = rewardMark
+    def __str__(self):
+        return str(self.name) + " " + str(self.rewardMark)
 # people
 people = [{'name':'chandan','age':20,'salary':2000},
 {'name':'chetan','age':18,'salary':5000},
@@ -24,10 +29,11 @@ by_salary = itemgetter('salary')
 people.sort(key=by_age) #in-place sorting by age
 people.sort(key=by_salary) #in-place sorting by salary
 # person
-person = Person()
+person = Person("Minh")
 print(person.__str__())
 Person.countPerson()
 Person.countPerson()
 Person.countPerson()
 print(Person.getCount())
-
+specialPerson = SpecialPerson("hoa",43)
+print(specialPerson.__str__())
