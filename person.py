16bc96c4fadf7229cb3e213f7e8d945436bc9912
phithai <phithai289@gmail.com>
2020-07-11 21:57:17 +0700

    @static method

--- person.py
+++ person.py
@@ -1,12 +1,20 @@
 from operator import itemgetter,attrgetter
 class Person:
     """a simple class"""
+    @classmethod
     def __init__(self):
         self.name = "default"
+        Person.count = 0
     def __str__(self):
         return self.name
     def rename(self,name):
         self.name = name
+    @staticmethod
+    def countPerson():
+        Person.count+=1
+    @staticmethod
+    def getCount():
+        return Person.count
 # people
 people = [{'name':'chandan','age':20,'salary':2000},
 {'name':'chetan','age':18,'salary':5000},
@@ -18,3 +26,8 @@ people.sort(key=by_salary) #in-place sorting by salary
 # person
 person = Person()
 print(person.__str__())
+Person.countPerson()
+Person.countPerson()
+Person.countPerson()
+print(Person.getCount())
+
