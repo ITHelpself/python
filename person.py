5fbb86ead8bd399d35dffd0d945d9382f9796919
phithai <phithai289@gmail.com>
2020-07-11 21:44:26 +0700

    # class

--- person.py
+++ person.py
@@ -1,4 +1,13 @@
 from operator import itemgetter,attrgetter
+class Person:
+    """a simple class"""
+    def __init__(self):
+        self.name = "default"
+    def __str__(self):
+        return self.name
+    def rename(self,name):
+        self.name = name
+# people
 people = [{'name':'chandan','age':20,'salary':2000},
 {'name':'chetan','age':18,'salary':5000},
 {'name':'guru','age':30,'salary':3000}]
@@ -6,3 +15,6 @@ by_age = itemgetter('age')
 by_salary = itemgetter('salary')
 people.sort(key=by_age) #in-place sorting by age
 people.sort(key=by_salary) #in-place sorting by salary
+# person
+person = Person()
+print(person.__str__())
