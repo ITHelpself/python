2eb178cc53a6d5a7a8be489450a654d148ab3f85
phithai <phithai289@gmail.com>
2020-07-12 08:08:00 +0700

    process list html by bs4 process table html by pyquery

--- htmlProcess.py
+++ htmlProcess.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from pyquery import PyQuery
 data = """
     <ul>
         <li class="item">item1</li>
@@ -9,3 +10,34 @@ data = """
 soup = BeautifulSoup(data, 'html.parser')
 for item in soup.select('li.item'):
     print(item.get_text())
+# PyQuery
+html = """
+    <h1>Sales</h1>
+    <table id="table">
+    <tr>
+        <td>Lorem</td>
+        <td>46</td>
+        </tr>
+    <tr>
+        <td>Ipsum</td>
+        <td>12</td>
+        </tr>
+        <tr>
+        <td>Dolor</td>
+        <td>27</td>
+    </tr>
+    <tr>
+        <td>Sit</td>
+        <td>90</td>
+    </tr>
+    </table>
+    """
+doc = PyQuery(html)
+title = doc('h1').text()
+print(title)
+table_data = []
+rows = doc('#table >tr')
+for row in rows:
+    name = PyQuery(row).find('td').eq(0).text()
+    value = PyQuery(row).find('td').eq(1).text()
+    print("({},{})".format(name, value))
